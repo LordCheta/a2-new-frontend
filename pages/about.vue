09d51a3a07de8f7865e98896b083bf82d85818a4
yvcheta <cheta@youverify.co>
2022-01-07 20:45:22 +0100

    fear: complete mission section of about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,71 @@
 <template>
+  <div class="h-screen">
+    <section class="bg-purple-50 p-40">
+      <h2 class="font-black text-5xl text-a2-blue">Our Mission</h2>
+
+      <section class="mt-40 flex flex-wrap gap-x-40 gap-y-20">
+        <div class="flex flex-row h-48 w-5/12 shadow-md">
+          <div class="bg-a2blue flex justify-center items-center w-2/12">
+            <img class="w-10 h-10" src="~/assets/images/target-white.svg" alt="white target icon">
+          </div>
+          <div class="p-8 bg-indigo-50 w-full">
+            <p class="text-xl font-bold text-a2blue">Mission One</p>
+
+            <p class="text-md mt-4 text-gray-500">To create a super platform that everyone can plug into and earn.</p>
+          </div>
+        </div>
+
+        <div class="flex flex-row h-48 w-5/12 shadow-md">
+          <div class="bg-a2yellow flex justify-center items-center w-2/12">
+            <img class="w-10 h-10" src="~/assets/images/target-white.svg" alt="white target icon">
+          </div>
+          <div class="p-8 bg-indigo-50 w-full">
+            <p class="text-xl font-bold text-a2blue">Mission Two</p>
+
+            <p class="text-md mt-4 text-gray-500">To empower businesses through technology.</p>
+          </div>
+        </div>
+
+        <div class="flex flex-row h-48 w-5/12 shadow-md">
+          <div class="bg-a2blue flex justify-center items-center w-2/12">
+            <img class="w-10 h-10" src="~/assets/images/target-white.svg" alt="white target icon">
+          </div>
+          <div class="p-8 bg-indigo-50 w-full">
+            <p class="text-xl font-bold text-a2blue">Mission Three</p>
+
+            <p class="text-md mt-4 text-gray-500">To try out new ground breaking marketing strategies.</p>
+          </div>
+        </div>
+
+        <div class="flex flex-row h-48 w-5/12 shadow-md">
+          <div class="bg-a2yellow flex justify-center items-center w-2/12">
+            <img class="w-10 h-10" src="~/assets/images/target-white.svg" alt="white target icon">
+          </div>
+          <div class="p-8 bg-indigo-50 w-full">
+            <p class="text-xl font-bold text-a2blue">Mission Four</p>
+
+            <p class="text-md mt-4 text-gray-500">To reward people for doing the same things they do everyday.</p>
+          </div>
+        </div>
+      </section>
+    </section>
+
+    <section class="">
+      <h2>History</h2>
+      <p>
+      A2Mall started in February 2018, intending to try out a new marketing strategy by building relationships. The possibility of creating a business out of a passion for looking good.
+
+ A2Mall was all about connectivity and grew to 11 states in the country, supplying quality clothing and making friends.
+
+ 2019 brought A2Mall to base in Lagos as we tried to create a bigger community, making friends and improving our looks.
+
+ 2020 A2Mall concentrated more on creating more friends and growing the community, customer satisfaction and Sales.
+
+ 2021 A2Mall started its  E-commerce business, to empower people and give them an extra means of income by doing the same things they do every day.
+    </p>
+    </section>
+
+  </div>
 </template>
 
 <script>
