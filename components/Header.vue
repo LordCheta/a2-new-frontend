3ceb441b638b1b0a1ae13dacf71f116f7eb17305
yvcheta <cheta@youverify.co>
2022-04-01 09:06:49 +0100

    wip: remove border around menu hamburger icon

--- components/Header.vue
+++ components/Header.vue
@@ -6,7 +6,7 @@
         <img src="~/assets/images/a2logonew-white.png" class="w-44 h-18"/>
       </NuxtLink>
 
-      <button @click="toggleMobileMenu()" class="flex items-center px-3 py-2 border rounded text-a2yellow border-teal-400 hover:text-white hover:border-white">
+      <button @click="toggleMobileMenu()" class="flex items-center px-3 py-2 rounded text-a2yellow border-teal-400 hover:text-white hover:border-white">
         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </button>
     </section>
@@ -17,7 +17,7 @@
           <img src="~/assets/images/a2logonew-white.png" class="w-44 h-18"/>
         </NuxtLink>
 
-        <button @click="toggleMobileMenu()" class="flex items-center px-3 py-2 border rounded text-a2yellow border-teal-400 hover:text-white hover:border-white lg:hidden">
+        <button @click="toggleMobileMenu()" class="flex items-center px-3 py-2 rounded text-a2yellow border-teal-400 hover:text-white hover:border-white lg:hidden">
           <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
         </button>
       </div>
@@ -32,7 +32,7 @@
         <Cart class="m-4"/>
       </div>
 
-      
+
 
     </section>
   </header >
